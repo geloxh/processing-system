9e8a476245c4670c29691df8ecd8333c00a789eb
geloxh <ace.blazado7@gmail.com>
2026-03-21 18:36:11 +0800

    csrf

--- views/auth/login.php
+++ views/auth/login.php
@@ -9,14 +9,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous"
+    >
+
 </head>
 <body class="bg-light d-flex align-items-center justify-content-center vh-100">
     <div class="card shadow p-4" style="width:360px">
         <h5 class="mb-3 text-center">Processing System</h5>
         <form method="POST" action="/processing-system/public/login">
-            \App\Helpers\Csrf::field();
+            <?= \App\Helpers\Csrf::field(); ?>
             <div class="mb-3">
                 <label class="form-label">Email</label>
                 <input type="email" name="email" class="form-control" required autofocus>
