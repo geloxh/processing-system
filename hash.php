ead68794cc5c7ece520ace27491c3628e828f9b2
geloxh <ace.blazado7@gmail.com>
2026-04-01 09:04:40 +0800

    Apply gitignore rules

--- hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo password_hash('Keepreinventing7*', PASSWORD_BCRYPT, ['cost' => 12]);
-    echo password_hash('3Ehitech*', PASSWORD_BCRYPT, ['cost' => 12]);
-?>
-
-"C:\xampp\php\php.exe" -r "echo password_hash('3Ehitech*', PASSWORD_BCRYPT, ['cost' => 12]);"
